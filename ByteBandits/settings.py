9553a3f208911bca9a402eb74cb6c8c9ae94c086
Ghasty101 <4231230250@live.gctu.edu.gh>
2024-08-08 23:41:47 +0000

    update

--- ByteBandits/settings.py
+++ ByteBandits/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--f&q^_#@yy7%bj0(zt#^g=^3t+uu2h!5z6v!sr#stvhs1a1-h9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
